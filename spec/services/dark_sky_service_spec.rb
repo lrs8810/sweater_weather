2c019b3f53ff23471d39f24ba8923412c4183ff6
Laura Schulz <lrs8810@gmail.com>
2020-01-12 22:34:52 -0700

    Remove extra line

--- spec/services/dark_sky_service_spec.rb
+++ spec/services/dark_sky_service_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-
 RSpec.describe DarkSkyService do
   it 'can get forecast' do
     data = DarkSkyService.get_forecast(39.7392358, -104.990251)
--- spec/services/google_geocode_service_spec.rb
+++ spec/services/google_geocode_service_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-
 RSpec.describe GoogleGeocodeService do
   it 'can get latitude and longitude of location' do
     data = GoogleGeocodeService.get_geocode('Denver,CO')
