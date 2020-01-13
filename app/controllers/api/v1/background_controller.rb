a423a72a195d51f340f94921171168b5c5e659ae
Laura Schulz <lrs8810@gmail.com>
2020-01-12 23:13:18 -0700

    Refactor show action

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -2,8 +2,7 @@ class Api::V1::BackgroundController < ApplicationController
 
   def show
     background_facade = BackgroundFacade.new(params[:location])
-    data = background_facade.get_background
-    render json: BackgroundSerializer.new(data)
+    render json: BackgroundSerializer.new(background_facade)
   end
 
 end
