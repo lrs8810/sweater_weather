73c62a4412366725a81f72b296109ceceb9136db
Laura Schulz <lrs8810@gmail.com>
2020-01-13 08:53:54 -0700

    Remove memoization

--- app/facades/background_facade.rb
+++ app/facades/background_facade.rb
@@ -1,6 +1,6 @@
 class BackgroundFacade
   def initialize(location)
-    @service ||= UnsplashService.get_background_json(location)
+    @service = UnsplashService.get_background_json(location)
   end
 
   def get_background
