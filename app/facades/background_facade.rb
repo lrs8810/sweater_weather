6a05601dea7753a327c5a67d60abf8eaaf0ec185
Laura Schulz <lrs8810@gmail.com>
2020-01-13 07:38:54 -0700

    Add .get_background method to BackgroundFacade

--- app/facades/background_facade.rb
+++ app/facades/background_facade.rb
@@ -1,5 +1,9 @@
 class BackgroundFacade
   def initialize(location)
-    @service ||= UnsplashService.get_background(location)
+    @service ||= UnsplashService.get_background_json(location)
+  end
+
+  def get_background
+    Background.new(@service)
   end
 end
