571fd8ea79f219d550a6e1b7589ddea58b1b8f17
Laura Schulz <lrs8810@gmail.com>
2020-01-13 07:38:33 -0700

    Refactor BackgroundController by calling .get_background method on background_facade

--- app/controllers/api/v1/background_controller.rb
+++ app/controllers/api/v1/background_controller.rb
@@ -2,7 +2,8 @@ class Api::V1::BackgroundController < ApplicationController
 
   def show
     background_facade = BackgroundFacade.new(params[:location])
-    render json: BackgroundSerializer.new(background_facade)
+    background = background_facade.get_background
+    render json: BackgroundSerializer.new(background)
   end
 
 end
