420161c17a6e858851179316bc6b3514de21a657
Laura Schulz <lrs8810@gmail.com>
2020-01-13 11:07:53 -0700

    Add cassette and refactor antipode request spec

--- spec/requests/api/v1/antipode_spec.rb
+++ spec/requests/api/v1/antipode_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 
 describe 'Antipode API' do
   it "it sends a resonse with the antipode city's info", :vcr do
-    WebMock.enable_net_connect!
-    VCR.eject_cassette
-    VCR.turn_off!(ignore_cassettes: true)
 
     get '/api/v1/antipode?location=hongkong'
 
-    expect(response).to be_successful
-    #
-    # response = response.body
+    last_response = response
+
+    expect(last_response.status).to eq(200)
+
+    json = JSON.parse(last_response.body, symbolize_names: true)
+
+    expect(json[:data][:attributes].length).to eq(3)
+
+    expect(json[:data][:attributes][:forecast].length).to eq(2)
   end
 end
