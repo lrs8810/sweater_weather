7f2eeb84c4907c3b51a4b0de7155e2e5692d72e4
Laura Schulz <lrs8810@gmail.com>
2020-01-13 09:25:39 -0700

    Comment out response.body

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe 'Forecast API', :type => :request do
   it "it sends all the forecast details for a city", :vcr do
     get '/api/v1/forecast?location=denver,co'
-    
+
     expect(response).to be_successful
 
-    response = response.body
+    # response = response.body
   end
 end
