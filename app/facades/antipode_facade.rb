ed2a9de9575d3219043713f00c4d856d6ccdb9d9
Laura Schulz <lrs8810@gmail.com>
2020-01-13 10:19:07 -0700

    Add AntipodeFacade

---
+++ app/facades/antipode_facade.rb
@@ -0,0 +1,12 @@
+class AntipodeFacade
+  def initialize(location)
+    @origin_geocode = GoogleGeocodeService.get_geocode(location)
+    @antipode = AmypodeService.get_antipode_lat_long(@origin_geocode[:lat], @origin_geocode[:long])
+    @antipode_geocode = GoogleGeocodeService.get_reverse_geocode(@antipode[:lat], @antipode[:long])
+    @forecast_details = DarkSkyService.get_forecast(@antipode[:lat], @antipode[:long])
+  end
+
+  def get_antipode_response
+    Antipode.new(@forecast_details, @origin_geocode, @antipode_geocode)
+  end
+end
