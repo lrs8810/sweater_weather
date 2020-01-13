2eb7fe0e07dbcce0943b3380aa8ce342534fec4d
Laura Schulz <lrs8810@gmail.com>
2020-01-13 11:49:27 -0700

    Refactor initialize method with key :long

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,7 +1,7 @@
 class ForecastFacade
   def initialize(location)
     @geocode = GoogleGeocodeService.get_geocode(location)
-    @forecast_details = DarkSkyService.get_forecast(@geocode[:lat], @geocode[:lng])
+    @forecast_details = DarkSkyService.get_forecast(@geocode[:lat], @geocode[:long])
   end
 
   def get_forecast_details
