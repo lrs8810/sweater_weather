879fdcbe022633d7d7c251b295d29d90d3ae944b
Laura Schulz <lrs8810@gmail.com>
2020-01-13 09:25:49 -0700

    Add AntipodeController

---
+++ app/controllers/api/v1/antipode_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::AntipodeController < ApplicationController
+
+  def show
+    # background_facade = BackgroundFacade.new(params[:location])
+    # background = background_facade.get_background
+    # render json: BackgroundSerializer.new(background)
+  end
+
+end
