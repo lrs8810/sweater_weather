1698bc4a9011f5bb548033aaa0aa302ec5630005
Laura Schulz <lrs8810@gmail.com>
2020-01-13 11:48:04 -0700

    Refactor forecast request spec

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -4,8 +4,20 @@ describe 'Forecast API', :type => :request do
   it "it sends all the forecast details for a city", :vcr do
     get '/api/v1/forecast?location=denver,co'
 
-    expect(response).to be_successful
+    last_response = response
 
-    # response = response.body
+    expect(last_response.status).to eq(200)
+
+    json = JSON.parse(last_response.body, symbolize_names: true)
+
+    expect(json[:data][:attributes][:results].length).to eq(2)
+
+    expect(json[:data][:attributes][:results][:current_temp].length).to eq(2)
+
+    expect(json[:data][:attributes][:results][:hourly_daily].length).to eq(2)
+
+    expect(json[:data][:attributes][:results][:hourly_daily][:hourly].length).to eq(8)
+
+    expect(json[:data][:attributes][:results][:hourly_daily][:daily].length).to eq(5)
   end
 end
