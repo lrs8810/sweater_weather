d7f75c7c482dee34cb866856ed452197d805e17e
Laura Schulz <lrs8810@gmail.com>
2020-01-14 16:13:26 -0700

    Add additional test to login request spec

--- spec/requests/api/v1/user_login_spec.rb
+++ spec/requests/api/v1/user_login_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'User Login API' do
   it 'can login a registered user' do
-    User.create(email: 'whatever@example.com',
+    user = User.create(email: 'whatever@example.com',
                 password: 'password',
                 password_confirmation: 'password')
 
@@ -24,6 +24,8 @@ describe 'User Login API' do
     response_body = JSON.parse(last_response.body, symbolize_names: true)
 
     expect(response_body[:data][:attributes]).to have_key(:api_key)
+
+    expect(response_body[:data][:attributes][:api_key]).to eq(user.api_key)
   end
 
   it 'cannot create a new user if all fields are not present' do
