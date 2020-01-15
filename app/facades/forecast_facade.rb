9fad4d8879f0144036ea02e28a790b498cff108b
Laura Schulz <lrs8810@gmail.com>
2020-01-15 09:53:53 -0700

    Refactor ForecastFacade and ForecastController with forecast_object method

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,11 +1,17 @@
 class ForecastFacade
   def initialize(location)
-    @geocode_json = GoogleService.get_geo_json(location)
-    @coords = CoordinateFormatter.new(@geocode_json)
-    @forecast_json = DarkSkyService.get_forecast(@coords.lat, @coords.long)
+    @geocode_json_data = GoogleService.get_geo_json(location)
+    @coords = CoordinateFormatter.new(@geocode_json_data)
+    @forecast_json_data = DarkSkyService.get_forecast(@coords.lat, @coords.long)
   end
 
-  def get_forecast_details
-    Forecast.new(@geocode_json, @forecast_json)
+  def forecast_object
+    Forecast.new(geocode_city_info, @forecast_json_data)
+  end
+
+  private
+
+  def geocode_city_info
+    CityInfoFormatter.new(@geocode_json_data)
   end
 end
