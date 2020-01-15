a48d204be58ab88ff9f70931da5563b4230b2737
Laura Schulz <lrs8810@gmail.com>
2020-01-15 06:27:51 -0700

    Refactor instance variables to into _json in variable name

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,10 +1,11 @@
 class ForecastFacade
   def initialize(location)
-    @geocode = GoogleService.get_geo_json(location)
-    @forecast_details = DarkSkyService.get_forecast(@geocode[:lat], @geocode[:long])
+    @geocode_json = GoogleService.get_geo_json(location)
+    @coords = CoordinateFormatter.new(@geocode_json)
+    @forecast_json = DarkSkyService.get_forecast(@coords.lat, @coords.long)
   end
 
   def get_forecast_details
-    Forecast.new(@geocode, @forecast_details)
+    Forecast.new(@geocode_json, @forecast_json)
   end
 end
