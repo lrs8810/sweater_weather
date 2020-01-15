db4d55713d50384c721caa142be50de56c781a6d
Laura Schulz <lrs8810@gmail.com>
2020-01-14 17:40:36 -0700

    Remove require(:user) from strong params and create user_error_message method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,12 +5,18 @@ class Api::V1::UsersController < ApplicationController
     if user.save
       render json: UserSerializer.new(user), status: 201
     else
-      render json: user.errors.full_messages.uniq.to_sentence, status: 400
+      render json: user_error_message(user), status: 400
     end
   end
 
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.permit(:email, :password, :password_confirmation)
+  end
+
+  def user_error_message(user)
+    {
+      error: user.errors.full_messages.uniq.to_sentence
+    }
   end
 end
